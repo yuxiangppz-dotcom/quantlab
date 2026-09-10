5f13e2060a17ca5dbb12f036f3ab5071ad741bb8
yuxiangppz-dotcom <yuxiang.ppz@gmail.com>
2026-09-10 13:00:01 +0800

    research: export strategy registry lifecycle

--- src/quantlab/research/__init__.py
+++ src/quantlab/research/__init__.py
@@ -15,22 +15,46 @@ from quantlab.research.evaluation import (
 from quantlab.research.models import ResearchDailyPrice
 from quantlab.research.price import build_prices, filter_point_in_time, prices_to_frame
 from quantlab.research.returns import calculate_forward_returns, calculate_returns
+from quantlab.research.strategy_registry import (
+    ELIGIBLE_FOR_USER_REVIEW,
+    FORWARD_EVIDENCE_ACCUMULATING,
+    FORWARD_SHADOW,
+    IDEA,
+    PORTFOLIO_AUDITED,
+    REJECTED,
+    RESEARCHED,
+    USER_APPROVED,
+    StrategyRegistryEntry,
+    load_strategy_registry,
+    transition_strategy,
+)
 from quantlab.research.universe import filter_v1_universe
 
 __all__ = [
     "ALPHA158_EXACT_SUBSET",
-    "alpha158_subset_rows",
-    "calculate_alpha158_exact_subset",
+    "ELIGIBLE_FOR_USER_REVIEW",
+    "FORWARD_EVIDENCE_ACCUMULATING",
+    "FORWARD_SHADOW",
+    "IDEA",
+    "PORTFOLIO_AUDITED",
+    "REJECTED",
+    "RESEARCHED",
     "ResearchDailyPrice",
+    "StrategyRegistryEntry",
+    "USER_APPROVED",
+    "alpha158_subset_rows",
     "build_prices",
     "build_research_dataset",
+    "calculate_alpha158_exact_subset",
     "calculate_forward_returns",
     "calculate_returns",
     "daily_rank_ic",
     "filter_point_in_time",
     "filter_v1_universe",
+    "load_strategy_registry",
     "prices_to_frame",
     "quantile_returns",
     "summarize_ic",
     "summarize_quantiles",
+    "transition_strategy",
 ]
